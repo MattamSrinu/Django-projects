61b210be6c1289077f24c7e1064654486d48c7c2
MattamSrinu <mattamsrinu15@gmail.com>
2025-11-16 12:31:45 +0530

    hosted

--- my_friends_list/settings.py
+++ my_friends_list/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['django-projects-xrm6.onrender.com','localhost:8000']
 
 
 # Application definition
